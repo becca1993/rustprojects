278c394fc6ce111658fd178dd1618d9234a53dd9
becca1993 <rebeccalmartin16@gmail.com>
2017-10-26 22:11:59 +0100

    Uploading Rust practice file. This program is for seeing if a user can vote and what party they would vote for.

---
+++ src/main.rs
@@ -0,0 +1,27 @@
+fn main(){
+
+    #[macro_use] extern crate text_io;
+
+
+    let mut s = String::new();
+    scan!("{} is your name", s);
+    let mut age = i32;
+    scan!("{} is your age", age);
+    if age >= 18{
+        println!("You can vote.");
+        println!("What party would you like to vote for?");
+        let mut partychoice = String::new();
+            if partychoice == "LAB"{
+                println! ("Voting for Labour");
+            }else if partychoice == "CON"{
+                println! ("Voting for the Tories");
+            }else{
+                println("Party not recongised!");
+            }
+
+    }else{
+        println!("Too young to vote");
+    }
+
+
+}
